ceeb98c3eb6fed036f5a46b11e412a241ade35e9
Karl Friman <karl.friman@devoteam.com>
2022-06-04 08:27:51 +0200

    Npm Audit

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,13 @@
 import React from "react";
-import { useTheme, View, Flex, IconButton, Spacer } from "vcc-ui";
+import {
+  useTheme,
+  View,
+  Flex,
+  IconButton,
+  Spacer,
+  TextInput,
+  Block,
+} from "vcc-ui";
 import cars from "../public/api/cars.json";
 import { Car } from "../types";
 import Card from "../src/components/Card";
@@ -13,65 +21,72 @@ export default function HomePage() {
   const loadNext = (): void => {
     alert("not implemented yet!");
   };
-  const newcars = cars.filter((x) => x.id.includes("xc"));
   const [filter, setFilter] = useState("");
   return (
     <View
       extend={{
         background: theme.color.background.primary,
-        // transition: '3s opacity ease-in',
         paddingTop: "3rem",
         paddingLeft: "3rem",
         marginRight: "3rem",
         margin: "0",
-        display: "flex",
-        flexDirection: "column",
-        overflow: "hidden",
       }}
     >
-      <p>
-        Type to filter the list:
-        <input
-          id="filter"
-          name="filter"
-          type="text"
+      <Block
+        extend={{
+          paddingLeft: "1rem",
+        }}
+      >
+        <TextInput
           value={filter}
-          onChange={(event) => setFilter(event.target.value)}
+          label="Filter by model"
+          onChange={(e) => {
+            setFilter(e.target.value);
+          }}
+          style={{
+            width: "12rem",
+          }}
         />
-      </p>
+      </Block>
       <Flex
         extend={{
-          flexDirection: "row",
+          overflow: "hidden",
         }}
       >
-        {cars
-          .filter((f) => f.includes(filter) || filter === "")
-          .map((car: Car, index: number) => {
-            return <Card key={index} {...car}></Card>;
-          })}
-      </Flex>
+        <Flex
+          extend={{
+            flexDirection: "row",
+          }}
+        >
+          {cars
+            .filter((f) => f.modelName.toLowerCase().includes(filter.toLowerCase()) || filter === "")
+            .map((car: Car, index: number) => {
+              return <Card key={index} {...car}></Card>;
+            })}
+        </Flex>
 
-      <Flex
-        extend={{
-          flexDirection: "row",
-          padding: "1rem",
-          justifyContent: "end",
-          flex: "5",
-        }}
-      >
-        <IconButton
-          onClick={loadPrev}
-          aria-label="prev"
-          iconName="navigation-chevronback"
-          variant="outline"
-        />
-        <Spacer />
-        <IconButton
-          onClick={loadNext}
-          aria-label="next"
-          iconName="navigation-chevronforward"
-          variant="outline"
-        />
+        <Flex
+          extend={{
+            flexDirection: "row",
+            padding: "1rem",
+            justifyContent: "end",
+            flex: "5",
+          }}
+        >
+          <IconButton
+            onClick={loadPrev}
+            aria-label="prev"
+            iconName="navigation-chevronback"
+            variant="outline"
+          />
+          <Spacer />
+          <IconButton
+            onClick={loadNext}
+            aria-label="next"
+            iconName="navigation-chevronforward"
+            variant="outline"
+          />
+        </Flex>
       </Flex>
     </View>
   );
