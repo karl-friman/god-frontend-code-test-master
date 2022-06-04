a3f67707ab3a75cc127fd9e80c0ead74a2111dd5
Karl Friman <karl.friman@devoteam.com>
2022-06-04 07:52:10 +0200

    CSS breakpoints

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,48 +1,78 @@
-import React from 'react';
-import { Block, Button, Grid, useTheme, View, Row, Flex, Link } from 'vcc-ui';
-import cars from '../public/api/cars.json';
-import { Car } from '../types';
-import Card from '../src/components/Card';
+import React from "react";
+import { useTheme, View, Flex, IconButton, Spacer } from "vcc-ui";
+import cars from "../public/api/cars.json";
+import { Car } from "../types";
+import Card from "../src/components/Card";
+import { useState } from "react";
 
 export default function HomePage() {
   const theme = useTheme();
-
+  const loadPrev = (): void => {
+    alert("not implemented yet!");
+  };
+  const loadNext = (): void => {
+    alert("not implemented yet!");
+  };
+  const newcars = cars.filter((x) => x.id.includes("xc"));
+  const [filter, setFilter] = useState("");
   return (
     <View
       extend={{
         background: theme.color.background.primary,
         // transition: '3s opacity ease-in',
-        paddingTop: '3rem',
-        margin: '0',
-        display: 'flex',
-        flexDirection: 'column',
-        // overflow: 'hidden'
+        paddingTop: "3rem",
+        paddingLeft: "3rem",
+        marginRight: "3rem",
+        margin: "0",
+        display: "flex",
+        flexDirection: "column",
+        overflow: "hidden",
       }}
     >
+      <p>
+        Type to filter the list:
+        <input
+          id="filter"
+          name="filter"
+          type="text"
+          value={filter}
+          onChange={(event) => setFilter(event.target.value)}
+        />
+      </p>
       <Flex
         extend={{
-          flexDirection: 'row',
-          // flexWrap: 'wrap',
-
+          flexDirection: "row",
         }}
       >
-        {cars.map((car: Car, index: number) => {
-          return (
-              <Card key={index} {...car}></Card>
-          );
-        })}
+        {cars
+          .filter((f) => f.includes(filter) || filter === "")
+          .map((car: Car, index: number) => {
+            return <Card key={index} {...car}></Card>;
+          })}
       </Flex>
 
-      <View
-        spacing={2}
-        direction='row'
-        justifyContent='end'
+      <Flex
         extend={{
-          marginTop: '1rem',
+          flexDirection: "row",
+          padding: "1rem",
+          justifyContent: "end",
+          flex: "5",
         }}
       >
-        dots
-      </View>
+        <IconButton
+          onClick={loadPrev}
+          aria-label="prev"
+          iconName="navigation-chevronback"
+          variant="outline"
+        />
+        <Spacer />
+        <IconButton
+          onClick={loadNext}
+          aria-label="next"
+          iconName="navigation-chevronforward"
+          variant="outline"
+        />
+      </Flex>
     </View>
   );
 }
--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -1,5 +1,5 @@
 import Image from "next/image";
-import { Link, Message, Block, Text, Flex, View } from "vcc-ui";
+import { Link, Block, Text, Flex, View, Spacer } from "vcc-ui";
 import { Car } from "../../../types";
 
 export default function Card(props: Car): JSX.Element {
@@ -9,83 +9,93 @@ export default function Card(props: Car): JSX.Element {
     <View
       extend={{
         padding: "1rem",
-        width: '25%'
+        "@media (min-width: 2850px)": {
+            width: "20%",
+        },
+        "@media (min-width: 1400px)": {
+            width: "25%",
+        },
+        "@media (min-width: 720px)": {
+            width: "50%",
+        },
+        "@media (min-width: 500px)": {
+            width: "100%",
+        },
       }}
     >
-
-        <Block>
-          <Text
-            variant="columbus"
-            subStyle="emphasis"
-            extend={{ textTransform: "uppercase", color: "#707070" }}
-          >
-            {bodyType}
-          </Text>
-          <Flex
-            extend={{
-              flexDirection: "row",
-              paddingBottom: "1.5rem",
-              
-            }}
-          >
-            <Text
-              variant="hillary"
-              subStyle="emphasis"
-              extend={{ color: "#202020" }}
-            >
-              {modelName}&nbsp;
-            </Text>
-            <Text variant="hillary" extend={{ color: "#707070" }}>
-              {modelType}
-            </Text>
-          </Flex>
-        </Block>
-
-        <div
-          style={{
-            position: "relative",
-            aspectRatio: "4/3",
-            // "@media (min-width: 2560px)": {
-            //   height: "500px",
-            // },
-            // "@media (min-width: 1920px)": {
-            //   height: "400px",
-            // },
-            // "@media (min-width: 1600px)": {
-            //   height: "300px",
-            // },
-            // "@media (min-width: 1080px)": {
-            //   height: "230px",
-            // },
-          }}
+      <Block>
+        <Text
+          variant="columbus"
+          subStyle="emphasis"
+          extend={{ textTransform: "uppercase", color: "#707070" }}
         >
-          <Image src={imageUrl} alt={modelName} layout="fill" quality={100} />
-        </div>
-
+          {bodyType}
+        </Text>
         <Flex
           extend={{
             flexDirection: "row",
-            paddingTop: "4rem",
-            alignItems: "center",
-            justifyContent: "center",
+            paddingBottom: "1.5rem",
+            "@media (max-width: 720px)": {
+                flexDirection: "column",
+            },
           }}
         >
-          <Link
-            href={"/learn/" + id}
-            arrow="right"
-            style={{ fontSize: "1.8rem", padding: "2rem" }}
+          <Text
+            variant="hillary"
+            subStyle="emphasis"
+            extend={{ color: "#202020" }}
           >
-            LEARN
-          </Link>
-          <Link
-            href={"/shop/" + id}
-            arrow="right"
-            style={{ fontSize: "1.8rem", padding: "2rem" }}
-          >
-            SHOP
-          </Link>
+            {modelName}&nbsp;
+          </Text>
+          <Text variant="hillary" extend={{ color: "#707070" }}>
+            {modelType}
+          </Text>
         </Flex>
+      </Block>
 
+      <Block
+        style={{
+          position: "relative",
+          aspectRatio: "4/3",
+          // "@media (min-width: 2560px)": {
+          //   height: "500px",
+          // },
+          // "@media (min-width: 1920px)": {
+          //   height: "400px",
+          // },
+          // "@media (min-width: 1600px)": {
+          //   height: "300px",
+          // },
+          // "@media (min-width: 1080px)": {
+          //   height: "230px",
+          // },
+        }}
+      >
+        <Image src={imageUrl} alt={modelName} layout="fill" quality={100} />
+      </Block>
+
+      <Flex
+        extend={{
+          flexDirection: "row",
+          paddingTop: "2rem",
+          alignItems: "center",
+          justifyContent: "center",
+        }}
+      >
+        <Link
+          href={"/learn/" + id}
+          arrow="right"
+        >
+          LEARN
+        </Link>
+        <Spacer />
+        <Link
+          href={"/shop/" + id}
+          arrow="right"
+        >
+          SHOP
+        </Link>
+      </Flex>
     </View>
   );
 }
